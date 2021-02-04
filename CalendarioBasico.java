0ba1fd6e5b5a735e7c4498d12726cc10970cc5b9
oscar <oscarbarragan31@gmail.com>
2021-02-04 12:55:06 +0000

    0321.03 añadi un metodo para avazar la fecha del calendario y otro para devolverla

--- CalendarioBasico.java
+++ CalendarioBasico.java
@@ -2,16 +2,34 @@ public class CalendarioBasico{
     private int dia;
     private int mes;
     private int ano;
-    
+   
     public CalendarioBasico(){
         dia = 1;
         mes = 1;
         ano = 1;
-    } 
+    }   
     
     public String obtenerFecha(){
-       String fechaActual = dia + "-" + mes + "-" + ano;
-       return fechaActual;
+        String fechaActual;
+        String diaActual = "";
+        String mesActual = "";
+        String anoActual = "";
+        
+        if(dia <= 9){
+            diaActual = "0";
+        }
+        
+        if(mes <= 9){
+            mesActual = "0";
+        }
+        
+        if(ano <= 9){
+            anoActual = "0";
+        }
+        
+        fechaActual = (diaActual + dia + "-" + mesActual + mes + "-" + anoActual + ano);
+      
+        return fechaActual;
     }
     
     public void fijarFecha(int nuevoDia, int nuevoMes, int nuevoAno){
